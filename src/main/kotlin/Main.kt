2384340d38ba558ac080cf32b4eabf6148fbbfc1
ahadkhaan97 <ahad.sartaj.ew@gmail.com>
2023-02-03 17:00:20 +0500

    submitted

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,15 @@
 fun main() {
-    print(detectCycle(prepareList(intArrayOf(-1,-7,7,-4,19,6,-9,-5,-2,-5)))?.`val`)
+    print(detectCycle(prepareList(intArrayOf(-1, -7, 7, -4, 19, 6, -9, -5, -2, -5)))?.`val`)
 }
 
 fun detectCycle(head: ListNode?): ListNode? {
     val list = mutableSetOf<ListNode>()
     var temp = head
     while (temp != null) {
-        if (list.none { it.`val` == temp?.`val` }) {
+        if (list.none { it == temp }) {
             list.add(temp)
         } else {
-            //return list.indexOf(list.filter { it.`val` == temp?.`val` }[0])
+            return list.filter { it == temp }[0]
         }
 
         temp = temp.next
@@ -21,13 +21,18 @@ fun prepareList(array: IntArray): ListNode? {
     return if (array.isNotEmpty()) {
         val head = ListNode(array[0])
         head.next = null
+        var temp: ListNode? = null
+        var temp2: ListNode? = null
         for (i in 1 until array.size) {
-            var temp = head
-            while (temp.next != null) {
+            temp = head
+            while (temp?.next != null) {
+                if (temp.`val` == -5)
+                    temp2 = temp
                 temp = temp.next!!
             }
-            temp.next = ListNode(array[i])
+            temp?.next = ListNode(array[i])
         }
+        temp?.next = temp2
         head
     } else null
 }
